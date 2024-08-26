ee31d5db68001d72238deaa3c33a20af80c44954
Vivek Patel <90496331+Vivek04-05@users.noreply.github.com>
2024-08-26 17:21:27 +0530

    Update ChatGPTAPI.swift
    fix issues

--- Sources/ChatGPT-iOS/ChatGPTAPI.swift
+++ Sources/ChatGPT-iOS/ChatGPTAPI.swift
@@ -11,14 +11,14 @@ public class ChatGPTAPI {
     
     private let secureStorageManager = SecureStorageManager.shared
     
-    init(apiKey: String) {
+   public init(apiKey: String) {
         self.apiKey = apiKey
         self.organizationID = nil
         
         secureStorageManager.storeInKeychain(value: apiKey, key: Constants.ApiKey)
     }
     
-    init(apiKey: String, organizationID: String){
+   public init(apiKey: String, organizationID: String){
         self.apiKey = apiKey
         self.organizationID = organizationID
         
@@ -26,7 +26,7 @@ public class ChatGPTAPI {
         secureStorageManager.storeInKeychain(value: organizationID, key: Constants.OrganizationID)
     }
     
-    func getPromptResponse(prompt:String, completion: @escaping (Result<String, Error>) -> Void)
+  public func getPromptResponse(prompt:String, completion: @escaping (Result<String, Error>) -> Void)
     {
         let networkClass = NetworkClass()
         networkClass.getChatGPTResponse(prompt: prompt, completion: completion)
